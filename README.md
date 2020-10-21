c10e0353bd01233b68dcab7b90cb6fa9575a3f69
aliglaser <nooti777712@gmail.com>
2020-10-21 03:07:56 -0700

    Updates

---
+++ README.md
@@ -0,0 +1,7 @@
+# FATCAT
+
+An interactive game which tests users’ short-term memory… also there’s a cat!
+
+# Technologies
+
+FATCAT is built using JavaScript, HTML Canvas, CSS
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: "url-loader",
+      },
+    ],
+  },
+  devtool: "inline-source-map",
+  resolve: {
+    extensions: [".js", "*"],
+  },
+};
